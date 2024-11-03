5255efbf2a8eb99977edc213a04e78a7a546b0dc
Tuba-NL <ecrentuba@gmail.com>
2024-11-03 17:48:09 +0100

    Tuba deneme

--- src/main/java/DEnem2.java
+++ src/main/java/DEnem2.java
@@ -1,2 +1,5 @@
-package PACKAGE_NAME;public class DEnem2 {
+public class DEnem2 {
+    public static void main(String[] args) {
+        System.out.println("bakalim");
+    }
 }
---
+++ src/main/java/DEneme3.java
@@ -0,0 +1,6 @@
+public class DEneme3 {
+    public static void main(String[] args) {
+        System.out.println("deneme123"
+                );
+    }
+}
